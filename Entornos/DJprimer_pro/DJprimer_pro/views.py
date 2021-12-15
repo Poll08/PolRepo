9e48eb5e55d0ad291a73705109aa95d1152202db
Poll08 <pablobarrioscesar@gmail.com>
2021-12-14 23:44:51 -0300

    seguimos creando

--- Entornos/DJprimer_pro/DJprimer_pro/views.py
+++ Entornos/DJprimer_pro/DJprimer_pro/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render 
 
 def inicio(request):
-	return render(request, "inicio.html")
+	producto = [
+			{"nombre":"Computadora", "precio":120},
+			{"nombre":"Televisor", "precio":90}, 
+	]
+	
+	usuario={
+			"nombre":"Pablo"
+	}
+
+	context={
+			"usuario":usuario,
+			"producto":producto
+	}
+	return render(request, "inicio.html",context)
+
